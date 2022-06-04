0c6ae05aa277356fedf6cb5e00b95c13cd0dab5f
WHYTE100 <wchingwalu@gmail.com>
2022-06-04 03:32:28 -0700

    Third Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 40;
+
+	while (n < 50)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
